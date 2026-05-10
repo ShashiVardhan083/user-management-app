f5e48d19111ae33532e50f9d63fa9a050f16fa74
vardhan <shashi.podeti@cognine.com>
2026-05-10 21:00:28 +0530

    my name is added to through new branch

---
+++ UserManagementApp/Controllers/MyNameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagementApp.Controllers
+{
+    public class MyNameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
